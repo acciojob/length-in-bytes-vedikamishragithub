4e8b2589d0dac6e73de07aa471af718961194044
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-26 22:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	let blob = new Blob([str], {type: "text/plain"});
+    return blob.size;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
